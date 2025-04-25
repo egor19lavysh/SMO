59f644c3ba9993352010ea098ccaf4d240922fed
Egor Lavysh <egorlavys95@gmail.com>
2025-04-25 21:09:01 +0300

    refactoring

--- smo/smo.py
+++ smo/smo.py
@@ -14,59 +14,52 @@ class SMO:
         Множество точек P1
     p2 : list of int or nd.array
         Множество точек P2
-
-    Returns
-    -------
-    None
     """
 
-    def __init__(self):
+    def __init__(self, C: float = 1.0):
         self._p1 = None
         self._p2 = None
-        self._a = None
-        self._p = None
+        self._A = None
+        self._P = None
         self._m = None
         self._s = None
         self._n = None
         self._w = None
         self._u = None
         self._beta = None
-        self.c = 1
-        self.eta = None
+        self.ksi = None
+        self._C = C
 
-    def fit(self, x: list[float] | np.ndarray, y: list[float] | np.ndarray) -> None:
-        if isinstance(x, list):
-            x = np.array(x)
+    def fit(self, X: list[float] | np.ndarray, y: list[float] | np.ndarray) -> None:
+        if isinstance(X, list):
+            x = np.array(X)
         if isinstance(y, list):
             y = np.array(y)
-        if len(y) != len(x):
+        if len(y) != len(X):
             raise Exception("Длины векторов фичей и меток не совпадают")
 
-        p1 = x[y == 1]
-        p2 = x[y == -1]
+        p1 = X[y == 1]
+        p2 = X[y == -1]
 
         self._p1 = np.array(p1).T
         self._p2 = np.array(p2).T
-        self._a = np.hstack((self._p1, -self._p2))
-        self._p = np.vstack((p1, p2))
-        self._m = self._a.shape[1]
+        self._A = np.hstack((self._p1, -self._p2))
+        self._P = np.vstack((p1, p2))
+        self._m = self._A.shape[1]
         self._s = self._p1.shape[1]
         self._n = len(p1[0])
-
+        self._ksi = np.array([1 if i < self._s else -1 for i in range(self._m)])
         self._u, self._w = self._train()
         self._beta = self._compute_beta()
 
-    def predict(self, x: list[float] | np.ndarray) -> np.ndarray:
-        if isinstance(x, list):
-            x = np.array(x)
+    def predict(self, X: list[float] | np.ndarray) -> np.ndarray:
+        if isinstance(X, list):
+            x = np.array(X)
         if self._w is None or self._beta is None:
             raise Exception("Модель не обучена. Вызовите fit() сначала.")
 
-        decision = np.dot(x, self._w) + self._beta
+        decision = np.dot(X, self._w) + self._beta
         return np.where(decision >= 0, 1, -1)
-
-    def _ksi(self, i: int) -> int:
-        return 1 if i < self._s else -1
 
     def _get_indices(self, u) -> tuple[list[int], list[int]]:
         i_streak = list(range(self._s)) + [i for i in range(self._s, self._m) if u[i] > 0]
@@ -75,9 +68,9 @@ class SMO:
 
     def _get_i_min(self, v, i_streak) -> int:
         i_min = i_streak[0]
-        val_min = np.dot(v, self._p[i_min]) - self._ksi(i_min)
+        val_min = np.dot(v, self._P[i_min]) - self._ksi[i_min]
         for i in i_streak[1:]:
-            val = np.dot(v, self._p[i]) - self._ksi(i)
+            val = np.dot(v, self._P[i]) - self._ksi[i]
             if val < val_min:
                 val_min = val
                 i_min = i
@@ -86,9 +79,9 @@ class SMO:
 
     def _get_i_max(self, v, i_double_streak) -> int:
         i_max = i_double_streak[0]
-        val_max = np.dot(v, self._p[i_max]) - self._ksi(i_max)
+        val_max = np.dot(v, self._P[i_max]) - self._ksi[i_max]
         for i in i_double_streak[1:]:
-            val = np.dot(v, self._p[i]) - self._ksi(i)
+            val = np.dot(v, self._P[i]) - self._ksi[i]
             if val > val_max:
                 val_max = val
                 i_max = i
@@ -96,7 +89,7 @@ class SMO:
         return i_max
 
     def _compute_delta(self, v, i_min, i_max):
-        return np.dot(v, self._p[i_max] - self._p[i_min]) - (self._ksi(i_max) - self._ksi(i_min))
+        return np.dot(v, self._P[i_max] - self._P[i_min]) - (self._ksi[i_max] - self._ksi[i_min])
 
     def _compute_lambda_k(self, i_min: int, i_max: int, lambda_k_hat: float, u) -> float:
         s, m, n = self._s, self._m, self._n
@@ -112,21 +105,21 @@ class SMO:
 
     def _check_kkt(self, u, v, tol=1e-3):
         for i in range(self._m):
-            grad = np.dot(v, self._p[i]) - self._ksi(i)
+            grad = np.dot(v, self._P[i]) - self._ksi[i]
             if u[i] == 0 and grad < -tol:
                 return False
-            elif u[i] == self.c and grad > tol:
+            elif u[i] == self._C and grad > tol:
                 return False
-            elif 0 < u[i] < self.c and abs(grad) > tol:
+            elif 0 < u[i] < self._C and abs(grad) > tol:
                 return False
         return True
 
     def _compute_beta(self) -> float:
         # Случай 1: Используем опорные векторы (0 < u_i < C)
-        mask = (self._u > 0) & (self._u < self.c)
+        mask = (self._u > 0) & (self._u < self._C)
         if np.any(mask):
             beta_values = [
-                (1 / self._ksi(i)) - np.dot(self._w, self._p[i])
+                (1 / self._ksi[i]) - np.dot(self._w, self._P[i])
                 for i in np.where(mask)[0]
             ]
             return float(np.median(beta_values))
@@ -137,14 +130,13 @@ class SMO:
         return float(beta_grid[*np.argmin(losses)])
 
     def _compute_T(self, beta: float) -> float:
-        return sum([np.maximum(0, 1 - self._ksi(i) * (np.dot(self._p, self._w) + beta)) for i in range(self._m)])
+        return sum([np.maximum(0, 1 - self._ksi[i] * (np.dot(self._P, self._w) + beta)) for i in range(self._m)])
 
     def _train(self, max_iter: int = 1000) -> tuple[np.ndarray, np.ndarray]:
         s, m, n = self._s, self._m, self._n
-        ksi = np.array([1 if i < self._s else -1 for i in range(self._m)])
 
         u = np.zeros(m)
-        v = np.dot(self._a, u)
+        v = np.dot(self._A, u)
 
         for _ in range(max_iter):
 
@@ -157,28 +149,28 @@ class SMO:
             if delta_k <= 1e-6:
                 break
 
-            lambda_k_hat = delta_k / (np.linalg.norm(self._p[i_min] - self._p[i_max]) ** 2)
+            lambda_k_hat = delta_k / (np.linalg.norm(self._P[i_min] - self._P[i_max]) ** 2)
 
             lambda_k = self._compute_lambda_k(i_min, i_max, lambda_k_hat, u)
 
             u_new = u.copy()
 
-            u_new[i_min] += self._ksi(i_min) * lambda_k
-            u_new[i_max] -= self._ksi(i_max) * lambda_k
-            v += lambda_k * (self._p[i_min] - self._p[i_max])
+            u_new[i_min] += self._ksi[i_min] * lambda_k
+            u_new[i_max] -= self._ksi[i_max] * lambda_k
+            v += lambda_k * (self._P[i_min] - self._P[i_max])
 
-            u_new[i_min] = np.clip(u_new[i_min], 0, self.c)
-            u_new[i_max] = np.clip(u_new[i_max], 0, self.c)
+            u_new[i_min] = np.clip(u_new[i_min], 0, self._C)
+            u_new[i_max] = np.clip(u_new[i_max], 0, self._C)
 
-            current_sum = np.sum(ksi * u_new)
+            current_sum = np.sum(self._ksi * u_new)
             if abs(current_sum) > 1e-6:
-                delta = current_sum / (self._ksi(i_min) - self._ksi(i_max))
-                u_new[i_min] -= delta * self._ksi(i_min)
-                u_new[i_max] += delta * self._ksi(i_max)
-                u_new[i_min] = np.clip(u_new[i_min], 0, self.c)
-                u_new[i_max] = np.clip(u_new[i_max], 0, self.c)
+                delta = current_sum / (self._ksi[i_min] - self._ksi[i_max])
+                u_new[i_min] -= delta * self._ksi[i_min]
+                u_new[i_max] += delta * self._ksi[i_max]
+                u_new[i_min] = np.clip(u_new[i_min], 0, self._C)
+                u_new[i_max] = np.clip(u_new[i_max], 0, self._C)
 
-            v = np.dot(self._a, u_new)
+            v = np.dot(self._A, u_new)
             u = u_new
 
             if self._check_kkt(u, v):
